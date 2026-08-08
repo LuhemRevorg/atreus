320f5c73d55e9592c7c6c1092b00055ad65c1f5e
LuhemRevorg <mehulg8142@gmail.com>
2026-08-08 04:35:15 -0400

    Sessions, load model into memory when taking req

--- src/llama/llama.py
+++ src/llama/llama.py
@@ -1,13 +1,24 @@
 from ollama import chat
 from ollama import ChatResponse
 
+session_messages= [
+    {
+        'role': 'system',
+        'content': "You are Atreus, you will help me with my day to day to work, you have access to various tools available. Use them whenever needed."
+    }
+]
+
 def llama(message: str):
-    response: ChatResponse = chat(model="qwen3:8b", messages=[
+    # Users request a.k.a. me
+    session_messages.append(
         {
             'role': 'user',
-            'content': message,
+            'content': message
         }
-    ], think=False)
+    )
+
+    response: ChatResponse = chat(model="qwen3:8b", messages=session_messages)
+    session_messages.append(response['message'])
 
     return response.message.content
 
